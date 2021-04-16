dd2b2a26592bd98d0486d8746f424107ddeef417
Cyril VINH-TUNG <cyril@invitu.com>
2021-04-15 19:21:31 -1000

    [ADD] Add Product Categories from Revatua

---
+++ models/product.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api, fields, models, _
+
+
+class ProductCategory(models.Model):
+    _inherit = 'product.category'
+
+    id_revatua = fields.Char(string='Code Revatua', size=64,
+                             help='Code Revatua')
