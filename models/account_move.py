5ba021970d62d49e1ce7792cd2a9b07765132f3e
Cyril VINH-TUNG <cyril@invitu.com>
2021-07-08 14:01:59 -1000

    [FIX] fix discount calculation for invoice

--- models/account_move.py
+++ models/account_move.py
@@ -46,6 +46,6 @@ class AccountMoveLine(models.Model):
     def _get_price_total_and_subtotal_model(self, price_unit, quantity, discount, currency, product, partner, taxes, move_type):
         res = super(AccountMoveLine, self)._get_price_total_and_subtotal_model(price_unit=price_unit, quantity=quantity, discount=discount, currency=currency, product=product, partner=partner, taxes=taxes, move_type=move_type)
         if self.move_id.sale_type_id == self.env.ref('revatua_armateur.fret_sale_type'):
-            subtotal = round(quantity * price_unit * (1 - (discount or 0.0 / 100.0)), precision_digits=0, rounding_method='DOWN')
+            subtotal = round(quantity * price_unit * (1 - (discount / 100.0 or 0.0)), precision_digits=0, rounding_method='DOWN')
             res['price_total'] = res['price_subtotal'] = subtotal
         return res
