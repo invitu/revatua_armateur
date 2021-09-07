aa5c9c8142418ba30323db19fa2349e85cf23619
Cyril VINH-TUNG <cyril@invitu.com>
2021-09-06 17:00:56 -1000

    [IMP] We group dgae invoice by product_category before res_partner

--- report/dgae_invoices.py
+++ report/dgae_invoices.py
@@ -18,18 +18,19 @@ class DgaeInvoicesReport(models.AbstractModel):
 
     @api.model
     def _get_report_values(self, docids, data=None):
-        invoices = self._get_invoices(data)
+        categ_list = self._get_categ_list(data)
+
         return {
             "doc_model": "dgae.invoices.report.wizard",
             "data": data,
-            "invoices": invoices,
+            "categ_list": categ_list,
         }
 
-    def _get_invoices(self, data):
+    def _get_categ_list(self, data):
         """
-        Get list of invoices
+        Get list of invoices by categ_id
         """
-        invoices = []
+        categ_list = []
         dgae_id = self.env.ref('revatua_connector.partner_dgae').id
 
         # Select moves within date range
@@ -47,36 +48,65 @@ class DgaeInvoicesReport(models.AbstractModel):
                 ('state',  '=', 'posted')
             ])
 
-        # Get sales associated to selected moves
-        sales = self.env['sale.order'].search(
-            [('invoice_ids', 'in', move_ids.ids)])
+        # Get sales associated to selected moves sorted by date asc
+        sales = sorted(
+            self.env['sale.order'].search(
+                [('invoice_ids', 'in', move_ids.ids)]),
+            key=operator.attrgetter('voyage_id.date_depart'))
 
-        # Format list of invoices to return
         for sale in sales:
-            conn = {}
-            conn['numeroVoyage'] = sale.revatua_code
-            conn['dateVoyage'] = sale.voyage_id.date_depart
-            conn['destination'] = sale.ilearrivee_id.name
-            conn['destinataire'] = sale.partner_shipping_id.parent_id.name if sale.partner_shipping_id.parent_id.name else sale.partner_shipping_id.name
-            conn['numeroTahiti'] = sale.partner_id.vat
-            # montant du sale_order et non de account_move car on considère qu'à chaque sale_order, on a un account_move du même montant
-            conn['montant'] = int(sale.amount_untaxed)
+            # Check if categ_id exists in final list, else create it
+            categ_id = sale.order_line[0].product_id.categ_id
+            categ_exists = next((i for i, item in enumerate(
+                categ_list) if item['categ_id'] == categ_id), None)
 
-            for line in sale.order_line:
-                conn['qty'] = int(conn.get('qty', 0) + line.product_uom_qty)
-                conn['volume'] = float(conn.get('volume', 0) + line.volume)
-                conn['poids'] = float(conn.get('poids', 0) + line.poids)
-
-            partner_id = sale.partner_id.id
-            exists = next((i for i, item in enumerate(
-                invoices) if item['partner_id'] == partner_id), None)
-            if exists is not None:
-                invoices[exists]['connaissements'].append(conn)
+            if (categ_exists is not None):
+                categ_list[categ_exists]['invoices'] = self._get_invoices(
+                    sale, categ_list[categ_exists]['invoices'])
             else:
-                invoice = {}
-                invoice['partner_id'] = partner_id
-                invoice['expediteur'] = sale.partner_id.parent_id.name if sale.partner_id.parent_id.name else sale.partner_id.name
-                invoice['connaissements'] = [conn]
-                invoices.append(invoice)
+                new_categ = {}
+                new_categ['categ_id'] = categ_id
+                new_categ['invoices'] = self._get_invoices(
+                    sale, [])
+                categ_list.append(new_categ)
+
+        return categ_list
+
+    def _get_invoices(self, sale, invoices):
+        """
+        Return invoice's list
+        """
+        conn = self._get_conn_values(sale)
+        # Check if a partner_id's list already exists in invoice's list, else create it
+        partner_id = sale.partner_id.id
+        exists = next((i for i, item in enumerate(
+            invoices) if item['partner_id'] == partner_id), None)
+
+        if (exists is not None):
+            invoices[exists]['connaissements'].append(conn)
+        else:
+            invoice = {}
+            invoice['partner_id'] = partner_id
+            invoice['expediteur'] = sale.partner_id.parent_id.name if sale.partner_id.parent_id.name else sale.partner_id.name
+            invoice['connaissements'] = [conn]
+            invoices.append(invoice)
 
         return invoices
+
+    def _get_conn_values(self, sale):
+        """
+        Return an object with the connaissement's values
+        """
+        conn = {}
+        conn['numeroVoyage'] = sale.revatua_code
+        conn['dateVoyage'] = sale.voyage_id.date_depart
+        conn['destination'] = sale.ilearrivee_id.name
+        conn['destinataire'] = sale.partner_shipping_id.parent_id.name if sale.partner_shipping_id.parent_id.name else sale.partner_shipping_id.name
+        conn['numeroTahiti'] = sale.partner_id.vat
+        conn['qty'] = int(sale.order_line[0].product_uom_qty)
+        conn['volume'] = float(sale.order_line[0].volume)
+        conn['poids'] = float(sale.order_line[0].poids)
+        # taking sale_order's value instead of account_move for we consider each sale_order having the same value as account_move
+        conn['montant'] = int(sale.amount_untaxed)
+
+        return conn
